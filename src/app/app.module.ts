3fcef5f0801332e9acb368b30d2270ad07d715d1
iampratiktandel <iampratiktandel@gmail.com>
2021-11-21 16:00:56 +0530

    feat: add ngx-print

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { PrintPageComponent } from './print-page/print-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { NgxPrintModule } from 'ngx-print';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgxPrintModule],
   declarations: [
     AppComponent,
     PrintPageComponent,
